0cc19aadbc94cbb13ff1f04a1ddbfad3c42d3c11
asamir <asamir@ciena.com>
2021-07-07 18:54:50 +0200

    first commit from branch feature1 after revert

--- main.py
+++ main.py
@@ -12,5 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('samir')
+    print_hi('sprints_devops')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
